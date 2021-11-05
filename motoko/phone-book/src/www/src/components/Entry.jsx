67323ff8ae74572166dab5ec3969e3c524d3fc95
Sue Ann Kim <sueannkim@SueAnnKimsMBP.attlocal.net>
2021-11-04 17:20:30 -0700

    working insert lookup

--- motoko/phone-book/src/www/src/components/Entry.jsx
+++ motoko/phone-book/src/www/src/components/Entry.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-const Entry = () => {
+const Entry = ({ setEntry }) => {
   const [name, setName] = useState('');
   const [desc, setDesc] = useState('');
   const [phone, setPhone] = useState('');
@@ -8,26 +8,41 @@ const Entry = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     alert(`Entry for ${name} has been submitted`);
+    let input = {
+      name: name,
+      desc: desc,
+      phone: phone
+    }
+    setEntry(input);
   }
-  const handleChange = (e) => {
-    setName(e.target.value);
+  const handleChange = {
+    name: function (e) {
+      setName(e.target.value);
+    },
+    description: function (e) {
+      setDesc(e.target.value);
+    },
+    phoneNo: function (e) {
+      setPhone(e.target.value);
+    },
   }
 
   return (
     <div className="entry">
       Add Entry for Phonebook
       <form onSubmit={handleSubmit}>
-        <label>
-          Name
-          <input type="text" value={name} onChange={handleChange} />
-        </label>
-        <label>
-          Description
-          <input type="text" value={desc} onChange={handleChange} />
-        </label>
-        <label>
-          <input type="text" value={phone} onChange={handleChange} />
-        </label>
+        <div className="inputFields">
+          <label>Name</label>
+          <input type="text" value={name} onChange={handleChange.name} />
+        </div>
+        <div className="inputFields">
+          <label className="inputFields">Description</label>
+          <input type="text" value={desc} onChange={handleChange.description} />
+        </div>
+        <div className="inputFields">
+          <label className="inputFields">Phone No</label>
+          <input type="text" value={phone} onChange={handleChange.phoneNo} />
+        </div>
         <input type="submit" value="Submit" />
       </form>
     </div>
--- motoko/phone-book/src/www/src/index.jsx
+++ motoko/phone-book/src/www/src/index.jsx
@@ -11,17 +11,79 @@ class PhoneBook extends React.Component {
       name: '',
       desc: '',
       phone: '',
-    };
+      lookupName: '',
+      lookupDesc: '',
+      lookupPhone: '',
+    }
+    this.setEntry = this.setEntry.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.lookupName = this.lookupName.bind(this);
+  };
+
+  setEntry(input) {
+    this.setState({
+      name: input.name,
+      desc: input.desc,
+      phone: input.phone,
+    });
+
+    canister.insert(input.name, { desc: input.desc, phone: input.phone });
   }
+
+  handleChange(e) {
+    this.setState({
+      lookupName: e.target.value,
+    })
+  }
+
+  lookupName() {
+    canister.lookup(this.state.lookupName).then(opt_entry => {
+      console.log('here is the info after lookup');
+      console.log(opt_entry);
+      let entry = opt_entry.length > 0 ? opt_entry[0] : null;
+      if (entry === null || entry === undefined) {
+        entry = {
+          desc: "",
+          phone: "",
+        };
+      }
+      this.setState({
+        lookupDesc: entry.desc,
+        lookupPhone: entry.phone,
+      })
+    })
+  }
+
   render() {
+
     return (
       <div className="phonebook">
-        <Entry />
+        <Entry setEntry={this.setEntry} />
+        <div className="display">
+          Lookup info by Name
+          <input type="text" value={this.state.lookupName} onChange={this.handleChange} />
+          <button onClick={this.lookupName}>
+            Lookup
+          </button>
+          <div id="result">
+            {(!!this.state.lookupDesc === true) || (!!this.state.lookupPhone === true) ?
+              (
+                <div>
+                  <li>Name: {this.state.name}</li>
+                  <li>Description: {this.state.desc}</li>
+                  <li>Phone Number: {this.state.phone}</li>
+                </div>
+              )
+              : (
+                <div>no information found</div>
+              )
+            }
+          </div>
+
+        </div>
       </div>
     )
-
   }
-
 }
 
 ReactDOM.render(<PhoneBook />, document.getElementById('app'));
